1072e287be37f0d12f0f593a9e6261e98f9278d9
karthikpalnati15 <karthikpalnati123@gmail.com>
2026-03-16 13:04:29 +0530

    Modified .java & .class Files

--- Code.java
+++ Code.java
@@ -2,8 +2,14 @@ import java.util.*;
 public class Code {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a / b);
+        try {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a / b);
+        } catch (Exception e) {
+            System.out.println("Error: Division by zero or invalid input");
+        }
+        
+        
     }
 }
